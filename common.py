cd3fbc1d286caab817be3663e99f6d067b22d23c
297876771@qq.com <297876771@qq.com>
2018-06-07 13:54:06 +0800

    更新一部分

--- common.py
+++ common.py
@@ -1,7 +1,13 @@
 # -*- coding:utf-8 -*-
 import configparser
 import logging
+import os
+import sys
 import time
+import pydicom
+import pymysql
+from skimage import exposure, img_as_float
+import matplotlib.pyplot as plt
 
 from selenium import webdriver
 
@@ -73,3 +79,51 @@ def screen_web(httpUrl, savePath):
     brower.save_screenshot(savePath)
     print(u"---------图片已保存 : " + savePath + "-----------")
     brower.close()
+
+'''
+    mysql连接,连接池直接使用组件
+    标识结果集:
+    cursor = conn.cursor(cursor=pymysql.cursors.DictCursor)
+'''
+
+def get_mysql_conn(host='127.0.0.1', port=3306, user='root', passwd='root', db='test',charset='utf8'):
+    return pymysql.connect(host=host, port=port, user=user, passwd=passwd, db=db, charset=charset)
+
+'''
+    暂时存档,需要使用后提取
+    
+'''
+
+def dicom_2png(file):
+    _currFile = file
+    try:
+        dcm = pydicom.dcmread(file)
+        out_put_filelog("Dicom [error] : file : " + file, 'scanFile')
+        fileName = os.path.basename(file)
+        imageX = dcm.pixel_array
+        temp = imageX.copy()
+        print("shape ----", imageX.shape)
+        picMax = imageX.max()
+        vmin = imageX.min()
+        vmax = temp[temp < picMax].max()
+        # print("vmin : ", vmin)
+        # print("vmax : ", vmax)
+        imageX[imageX > vmax] = 0
+        imageX[imageX < vmin] = 0
+        # result = exposure.is_low_contrast(imageX)
+        # # print(result)
+        image = img_as_float(imageX)
+        plt.cla()
+        plt.figure('adjust_gamma', figsize=(10.24, 10.24))
+        plt.subplots_adjust(top=1, bottom=0, left=0, right=1, hspace=0, wspace=0)
+        plt.imshow(image, 'gray')
+        plt.axis('off')
+        plt.savefig(fileName + '.png')
+        time.sleep(1)
+    except Exception as e:
+        info = sys.exc_info()
+        out_put_filelog(
+            "Dicom [error] : file : " + _currFile + "--- error:" + str(e) + "track : " + str(info[0]) + ":" + str(
+                info[1]))
+    finally:
+        plt.close()
