835af8c936c7487d3eec2433651ba54bc4aeb755
297876771@qq.com <297876771@qq.com>
2018-07-16 15:47:54 +0800

    更新一下

--- FileSuffCheck.py
+++ FileSuffCheck.py
@@ -8,7 +8,7 @@ __authro__ = 'Bill'
 '''
     用于文件后缀解析判断
     @author:Bill
-    @Date: 2018/05/21
+    @Date: 2018/07/16
 '''
 
 
@@ -16,18 +16,21 @@ class FileSuffCheck(object):
 
     @staticmethod
     def file_sufis_png(file):
-        if os.path.splitext(file)[1].lower() == '.png':
+        suffix = os.path.splitext(file)[1].lower()
+        if suffix == '.png':
             return True
         return False
 
     @staticmethod
     def file_sufis_ini(file):
-        if os.path.splitext(file)[1].lower() == '.ini':
+        suffix = os.path.splitext(file)[1].lower()
+        if suffix == '.ini':
             return True
         return False
 
     @staticmethod
     def file_sufis_dcm(file):
-        if os.path.splitext(file)[1].lower() == '.dcm' or os.path.splitext(file)[1] == '.DCM':
+        suffix = os.path.splitext(file)[1].lower()
+        if suffix == '.dcm':
             return True
         return False
